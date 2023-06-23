eaf199cd30e08d5f506e9a4247cc4bd11e5b03dc
allen <allenmuhani@gamil.com>
2023-06-23 10:07:06 +0300

    close database connection

--- src/users/user.controller.spec.ts
+++ src/users/user.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UsersService } from './users.service';
-import { Neo4jModule } from 'nest-neo4j/dist';
+import { Neo4jModule, Neo4jService } from 'nest-neo4j/dist';
 import {
   DB_CONNECTIONS_CONFIGS,
   TestUtilsModule,
@@ -11,6 +11,7 @@ import { FirebaseTestUtilsService } from '../../test/test-utils/firebase-test-ut
 describe('User Controller', () => {
   let userController: UserController;
   let firebaseTestUtils: FirebaseTestUtilsService;
+  let dbService: Neo4jService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -20,11 +21,17 @@ describe('User Controller', () => {
     }).compile();
 
     userController = app.get<UserController>(UserController);
+    dbService = app.get<Neo4jService>(Neo4jService);
+
     firebaseTestUtils = app.get<FirebaseTestUtilsService>(
       FirebaseTestUtilsService,
     );
 
     const skey = await firebaseTestUtils.auth('test@gmail.com', 'test123');
+  });
+
+  afterAll(async () => {
+    dbService.getDriver().close();
   });
 
   it('should be defined', () => {
