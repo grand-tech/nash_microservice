eb664a9e8fcd6481868dc2c67f0776736f38c419
allen <allenmuhani@gamil.com>
2023-07-20 11:31:46 +0300

    initialize web3 utils before application start

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { initializeContractKit } from './utils/block-chain-utils/contract.kit.utils';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // make sure that the contract kit utils are initialized.
+  initializeContractKit();
+
   await app.listen(3000);
 }
 bootstrap();
