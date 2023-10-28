6b0fc4ccc7ef5d0df3137006e842853ac1269d22
allen <allenmuhani@gmail.com>
2023-10-28 12:24:41 +0300

    converted to fastify for speed purposes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { initializeContractKit } from './utils/block-chain-utils/contract.kit.utils';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
   // make sure that the contract kit utils are initialized.
   initializeContractKit();
 
