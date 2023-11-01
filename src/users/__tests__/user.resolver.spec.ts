be35625f5eb01b33a9f9eae9ae56b2093b7a6c16
allen <allenmuhani@gmail.com>
2023-11-01 11:09:26 +0300

    added graph ql endpoint tests

--- src/users/__tests__/user.resolver.spec.ts
+++ src/users/__tests__/user.resolver.spec.ts
@@ -22,21 +22,57 @@ let userService = {
     };
   },
   addPrivateKeyToAccount: () => {
+    const u = {
+      feduid: '1234567892',
+      email: 'testuser@gmail.com',
+      idNumber: '1234567890',
+      name: 'John Doe',
+      publicAddress: '1234567892',
+      phoneNumber: undefined,
+      privateKey: undefined,
+      publicKey: undefined,
+      mnemonic: undefined,
+      id: 0,
+      labels: []
+    }
     return {
       status: 200,
-      message: 'addPrivateKeyToAccount',
+      message: 'Success',
+      body: u
     };
   },
   addMnemonicToAccount: () => {
+    const u = {
+      feduid: '1234567892',
+      email: 'testuser@gmail.com',
+      idNumber: '1234567890',
+      name: 'John Doe',
+      publicAddress: '1234567892',
+      phoneNumber: undefined,
+      privateKey: undefined,
+      publicKey: undefined,
+      mnemonic: undefined,
+      id: 0,
+      labels: []
+    }
     return {
       status: 200,
-      message: 'addMnemonicToAccount',
+      message: 'Success',
+      body: u
     };
   },
   saveUserProfile: () => {
+    const u = {
+      name: 'John Doe',
+      publicAddress: '1234567892',
+      phoneNumber: '0712345678',
+      id: 0,
+      labels: []
+    }
     return {
       status: 200,
-      message: 'saveUserProfile',
+      message: 'Success',
+      body: u
     };
   },
   validateNewUser: () => {
@@ -88,39 +124,6 @@ describe('User Controller Mock Method Calls.', () => {
     await app.close();
   });
 
-  it(`Check if create new crypto wallet mutation exists.`, () => {
-    const mutation = `mutation {
-      createNewCryptoWallet(feduid: "1234567890" ) {
-        status
-        message
-        body {
-          name
-          idNumber
-          feduid
-          email
-        }
-      }
-    }`
-
-
-    return request(app.getHttpServer())
-      .post('/graphql')
-      .set("Authorization", skey)
-      .send(
-        {
-          query: mutation
-        }
-      )
-      .expect(200)
-      .expect((res) => {
-        const rsp = res.body.data.createNewCryptoWallet
-        expect(rsp.status).toBe(200)
-        expect(rsp.message).toBe('Success')
-
-        const body = rsp.body;
-        expect(body.feduid).toBe('1234567890')
-      });
-  }, 6000);
 
   it(`Test sign up mutation.`, () => {
 
@@ -157,33 +160,131 @@ describe('User Controller Mock Method Calls.', () => {
 
   }, 6000)
 
-  // it(`Check if add private key to account route exists.`, () => {
-  //   return request(app.getHttpServer())
-  //     .post('/add-privatekey-to-account')
-  //     .expect(201)
-  //     .expect(userService.addPrivateKeyToAccount());
-  // });
+  it(`Check if create new crypto wallet mutation exists.`, () => {
+    const mutation = `mutation {
+      addPrivateKeyToAccount(privateKey: "1234567890" ) {
+        status
+        message
+        body {
+          name
+          feduid
+          publicAddress
+        }
+      }
+    }`
 
-  // it(`Check if add mnemonic to account route exists.`, () => {
-  //   return request(app.getHttpServer())
-  //     .post('/add-mnemonic-to-account')
-  //     .expect(201)
-  //     .expect(userService.addMnemonicToAccount());
-  // });
+    return request(app.getHttpServer())
+      .post('/graphql')
+      .set("Authorization", skey)
+      .send(
+        {
+          query: mutation
+        }
+      )
+      .expect(200)
+      .expect((res) => {
+        const rsp = res.body.data.addPrivateKeyToAccount
+        expect(rsp.status).toBe(200)
+        expect(rsp.message).toBe('Success')
 
-  // it(`Check if save user profile route exists.`, () => {
-  //   return request(app.getHttpServer())
-  //     .post('/save-user-profile')
-  //     .expect(201)
-  //     .expect(userService.saveUserProfile());
-  // });
+        const body = rsp.body;
+        expect(body.feduid).toBe('1234567892')
+        expect(body.publicAddress).toBe('1234567892')
+      });
+  }, 6000);
 
-  // it(`Add mnemonic to account.`, () => {
-  //   return request(app.getHttpServer())
-  //     .post('/add-mnemonic-to-account')
-  //     .set('Authorization', `Bearer `)
-  //     .set('Content-Type', 'application/json')
-  //     .send({ mnemonic: '123 123 123' })
-  //     .expect(403);
-  // });
+  it(`Check if add private key to account wallet mutation exists.`, () => {
+    const mutation = `mutation {
+      createNewCryptoWallet(feduid: "1234567890" ) {
+        status
+        message
+        body {
+          name
+          idNumber
+          feduid
+          email
+        }
+      }
+    }`
+
+    return request(app.getHttpServer())
+      .post('/graphql')
+      .set("Authorization", skey)
+      .send(
+        {
+          query: mutation
+        }
+      )
+      .expect(200)
+      .expect((res) => {
+        const rsp = res.body.data.createNewCryptoWallet
+        expect(rsp.status).toBe(200)
+        expect(rsp.message).toBe('Success')
+
+        const body = rsp.body;
+        expect(body.feduid).toBe('1234567890')
+      });
+  }, 6000);
+
+  it(`Check if add mnemonic to account mutation exists.`, () => {
+    const mutation = `mutation {
+      addMnemonicToAccount(mnemonic: "1234567890") {
+        status
+        message
+        body {
+          publicAddress
+        }
+      }
+    }`
+
+    return request(app.getHttpServer())
+      .post('/graphql')
+      .set("Authorization", skey)
+      .send(
+        {
+          query: mutation
+        }
+      )
+      .expect(200)
+      .expect((res) => {
+        const rsp = res.body.data.addMnemonicToAccount
+        expect(rsp.status).toBe(200)
+        expect(rsp.message).toBe('Success')
+        const body = rsp.body;
+        expect(body.publicAddress).toBe('1234567892')
+      });
+  }, 6000);
+
+  it(`Check if save user profile mutation exists.`, () => {
+    const mutation = `mutation {
+      saveUserProfile(phoneNumber: "1234567890", fullName: "John Doe") {
+        status
+        message
+        body {
+          name
+          phoneNumber
+        }
+      }
+    }`
+
+    return request(app.getHttpServer())
+      .post('/graphql')
+      .set("Authorization", skey)
+      .send(
+        {
+          query: mutation
+        }
+      )
+      .expect(200)
+      .expect((res) => {
+        const rsp = res.body.data.saveUserProfile
+        expect(rsp.status).toBe(200)
+        expect(rsp.message).toBe('Success')
+
+        const body = rsp.body;
+        expect(body.phoneNumber).toBe('0712345678')
+        expect(body.name).toBe('John Doe')
+      });
+  }, 6000);
+
 });
--- src/users/user.resolver.ts
+++ src/users/user.resolver.ts
@@ -1,9 +1,10 @@
-import { Req } from '@nestjs/common';
+import { ExecutionContext, Req } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserResponse } from '../utils/response';
 import { User } from '../datatypes/user/user';
 import { Role, Roles } from '../utils/pre-auth/roles';
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { FastifyRequest } from 'fastify';
 
 @Resolver()
 export default class UserResolver {
@@ -31,48 +32,55 @@ export default class UserResolver {
     return await this.userService.validateNewUser(user);
   }
 
+  @Mutation(returns => UserResponse)
+  @Roles(Role.User)
+  async createNewCryptoWallet(
+    @Args('feduid') feduid: string,
+    @Context() context
+  ): Promise<UserResponse> {
+    const user: User = context.req.raw.user as User;
+    return await this.userService.createCryptoAccount(user);
+  }
+
+  @Mutation(returns => UserResponse)
+  @Roles(Role.User)
+  async addPrivateKeyToAccount(
+    @Args('privateKey') privateKey: string,
+    @Context() context
+  ): Promise<UserResponse> {
+    const user: User = context.req.raw.user as User;
+    return await this.userService
+      .addPrivateKeyToAccount(user, privateKey);
+  }
+
+  @Mutation(returns => UserResponse)
+  @Roles(Role.User)
+  async addMnemonicToAccount(
+    @Args('mnemonic') mnemonic: string,
+    @Context() context
+  ): Promise<UserResponse> {
+    const user: User = context.req.raw.user as User;
+    return await this.userService.addMnemonicToAccount(user, mnemonic);
+  }
+
+  @Mutation(returns => UserResponse)
+  @Roles(Role.User)
+  async saveUserProfile(
+    @Args('phoneNumber') phoneNumber: string,
+    @Args('fullName') fullName: string,
+    @Context() context
+  ): Promise<UserResponse> {
+    const user: User = context.req.raw.user as User;
+    return await this.userService.saveUserProfile(
+      user,
+      phoneNumber,
+      fullName,
+    );
+  }
+
   @Query(returns => [User])
   async getUsers() {
     return []
   }
 
-  @Mutation(returns => UserResponse)
-  @Roles(Role.User)
-  async createNewCryptoWallet(
-    @Args({ name: 'feduid', type: () => String })
-    @Req() request
-  ): Promise<UserResponse> {
-    const user: User = request['user'];
-    return await this.userService.createCryptoAccount(user);
-  }
-
-  // @Post('/add-privatekey-to-account')
-  // @Roles(Role.User)
-  // async addPrivateKeyToAccount(@Req() request: Request): Promise<Response> {
-  //   const user: User = request['user'];
-  //   const body = request.body;
-  //   return await this.userService.addPrivateKeyToAccount(user, body.privateKey);
-  // }
-
-  // @Post('/add-mnemonic-to-account')
-  // @Roles(Role.User)
-  // async addMnemonicToAccount(@Req() request: Request): Promise<Response> {
-  //   const user: User = request['user'];
-  //   const body = request.body;
-
-  //   return await this.userService.addMnemonicToAccount(user, body.mnemonic);
-  // }
-
-  // @Post('/save-user-profile')
-  // @Roles(Role.User)
-  // async saveUserProfile(@Req() request: Request): Promise<Response> {
-  //   const user: User = request['user'];
-  //   const body = request.body;
-
-  //   return await this.userService.saveUserProfile(
-  //     user,
-  //     body.phoneNumber,
-  //     body.fullName,
-  //   );
-  // }
 }
