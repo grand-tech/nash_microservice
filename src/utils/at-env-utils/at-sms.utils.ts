d056ad0c3f8aca4ca9468abd4ba524121928f730
allen <allenmuhani@gmail.com>
2024-06-26 11:46:31 +0300

    added sms utils

--- src/utils/at-env-utils/at-sms.utils.ts
+++ src/utils/at-env-utils/at-sms.utils.ts
@@ -15,19 +15,39 @@ const africastalking = AfricasTalking({
  * @param message the message to be sent.
  * @returns true if the message was sent successfully.
  */
-export async function sendSMS(to: string[], message: string) {
+export async function sendBulkSMS(to: string[], message: string) {
   // Send promotional message
   try {
-    const options = await africastalking.SMS.send({
+    const result = await africastalking.SMS.send({
       // list to be sourced from database
       to: to,
       message: message,
       from: '85036',
     });
 
+    console.log('Message sent successfully', result);
     return true;
   } catch (ex) {
     console.error(ex);
     return false;
+  }
+}
+
+/**
+ * Sends single recipient SMSs
+ * @param to the recipient of the sms.
+ * @param message the message body.
+ */
+export async function sendSMS(to: string, message: string) {
+  // Send promotional message
+  try {
+    const result = await africastalking.SMS.send({
+      to: to,
+      message: message,
+      from: '85036',
+    });
+    console.log('Message sent successfully', result);
+  } catch (ex) {
+    console.error(ex);
   }
 }
