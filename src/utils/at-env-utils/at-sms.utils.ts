48ee3517282f49cdfe87f68787f7b539a9e1bdbe
allen <allenmuhani@gmail.com>
2024-06-26 11:44:12 +0300

    refactored sms utils

--- src/utils/at-env-utils/at-sms.utils.ts
+++ src/utils/at-env-utils/at-sms.utils.ts
@@ -9,16 +9,25 @@ const africastalking = AfricasTalking({
   username: process.env.AFRICAS_TALKING_USERNAME,
 });
 
-module.exports = async function sendSMS(to: string, message: string) {
+/**
+ * Bulk messaging.
+ * @param to the recipients of the messages.
+ * @param message the message to be sent.
+ * @returns true if the message was sent successfully.
+ */
+export async function sendSMS(to: string[], message: string) {
   // Send promotional message
   try {
-    const result = await africastalking.SMS.send({
+    const options = await africastalking.SMS.send({
+      // list to be sourced from database
       to: to,
       message: message,
       from: '85036',
     });
-    console.log('Message sent successfully', result);
+
+    return true;
   } catch (ex) {
     console.error(ex);
+    return false;
   }
-};
+}
